390c233106ec0c08b64111b212e38135b500343a
Ellis Valentiner <ellisvalentiner@gmail.com>
2017-10-20 18:29:25 -0400

    Reorg. and Begin stubbing digital currency

--- src/AlphaVantage.jl
+++ src/AlphaVantage.jl
@@ -5,6 +5,25 @@ const alphavantage_api = "https://www.alphavantage.co/"
 using Requests
 using HttpCommon
 
-include("stocks.jl")
+function _get(uri::String)
+    resp = Requests.get(uri)
+    status = statuscode(resp)
+    if status != 200
+        desc = STATUS_CODES[status]
+        error("Expected status code 200 but received $status: $desc")
+    end
+    return resp.data
+end
+
+function _parse_data(data, datatype::String)
+    if datatype == "csv"
+        return readcsv(data)
+    elseif datatype == "json"
+        return Requests.json(data)
+    end
+end
+
+include("stock_time_series.jl.jl")
+export intraday, daily, daily_adjusted, weekly, weekly_adjusted, monthly, monthly_adjusted
 
 end # module
--- src/stock_time_series.jl
+++ src/stock_time_series.jl
@@ -1,23 +1,5 @@
 VERSION >= v"0.6.0"
 
-function _get(uri::String)
-    resp = Requests.get(uri)
-    status = statuscode(resp)
-    if status != 200
-        desc = STATUS_CODES[status]
-        error("Expected status code 200 but received $status: $desc")
-    end
-    return resp.data
-end
-
-function _parse_data(data, datatype::String)
-    if datatype == "csv"
-        return readcsv(data)
-    elseif datatype == "json"
-        return Requests.json(data)
-    end
-end
-
 function _validate_args(;kwargs...)
     args = Dict(kwargs)
     if !all(map(x->x in [:interval, :outputsize, :datatype], keys(args)))
