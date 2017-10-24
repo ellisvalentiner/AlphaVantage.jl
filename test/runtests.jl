0509adfd15a85992650e9e8e15c6aaf5abd6795d
Ellis Valentiner <ellisvalentiner@gmail.com>
2017-10-23 23:20:30 -0400

    More tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,66 @@
 using AlphaVantage
 using Base.Test
 
+@testset "Validate arguments" begin
+    @testset "Daily, weekly, monthly arguments" begin
+        @test AlphaVantage._validate_args(outputsize="compact", datatype="json") === nothing
+        @test AlphaVantage._validate_args(outputsize="compact", datatype="csv") === nothing
+        @test AlphaVantage._validate_args(outputsize="full", datatype="json") === nothing
+        @test AlphaVantage._validate_args(outputsize="full", datatype="csv") === nothing
+    end
+
+    @testset "Intraday arguments" begin
+        @testset "Interval: 1min" begin
+            @test AlphaVantage._validate_args(interval="1min", outputsize="compact", datatype="json") === nothing
+            @test AlphaVantage._validate_args(interval="1min", outputsize="compact", datatype="csv") === nothing
+            @test AlphaVantage._validate_args(interval="1min", outputsize="full", datatype="json") === nothing
+            @test AlphaVantage._validate_args(interval="1min", outputsize="full", datatype="csv") === nothing
+        end
+
+        @testset "Interval: 5min" begin
+            @test AlphaVantage._validate_args(interval="5min", outputsize="compact", datatype="json") === nothing
+            @test AlphaVantage._validate_args(interval="5min", outputsize="compact", datatype="csv") === nothing
+            @test AlphaVantage._validate_args(interval="5min", outputsize="full", datatype="json") === nothing
+            @test AlphaVantage._validate_args(interval="5min", outputsize="full", datatype="csv") === nothing
+        end
+
+        @testset "Interval: 15min" begin
+            @test AlphaVantage._validate_args(interval="15min", outputsize="compact", datatype="json") === nothing
+            @test AlphaVantage._validate_args(interval="15min", outputsize="compact", datatype="csv") === nothing
+            @test AlphaVantage._validate_args(interval="15min", outputsize="full", datatype="json") === nothing
+            @test AlphaVantage._validate_args(interval="15min", outputsize="full", datatype="csv") === nothing
+        end
+
+        @testset "Interval: 30min" begin
+            @test AlphaVantage._validate_args(interval="30min", outputsize="compact", datatype="json") === nothing
+            @test AlphaVantage._validate_args(interval="30min", outputsize="compact", datatype="csv") === nothing
+            @test AlphaVantage._validate_args(interval="30min", outputsize="full", datatype="json") === nothing
+            @test AlphaVantage._validate_args(interval="30min", outputsize="full", datatype="csv") === nothing
+        end
+
+        @testset "Interval: 60min" begin
+            @test AlphaVantage._validate_args(interval="60min", outputsize="compact", datatype="json") === nothing
+            @test AlphaVantage._validate_args(interval="60min", outputsize="compact", datatype="csv") === nothing
+            @test AlphaVantage._validate_args(interval="60min", outputsize="full", datatype="json") === nothing
+            @test AlphaVantage._validate_args(interval="60min", outputsize="full", datatype="csv") === nothing
+        end
+    end
+
+    @testset "Key errors" begin
+        @test_throws KeyError AlphaVantage._validate_args(outputsize="compact")
+        @test_throws KeyError AlphaVantage._validate_args(outputsize="full")
+        @test_throws KeyError AlphaVantage._validate_args(datatype="csv")
+        @test_throws KeyError AlphaVantage._validate_args(datatype="json")
+    end
+end
+
 @testset "Intraday data" begin
     @testset "Data format: CSV" begin
         data = intraday(apikey=ENV["ALPHA_VANTAGE_API_KEY"])
         @test typeof(data) === Array{Any, 2}
         @test size(data) === (101, 6)
     end
+
     @testset "Data format: JSON" begin
         data = intraday(datatype="json", apikey=ENV["ALPHA_VANTAGE_API_KEY"])
         @test typeof(data) === Dict{String,Any}
@@ -23,6 +77,7 @@ end
         @test typeof(data) === Array{Any, 2}
         @test size(data) === (101, 6)
     end
+
     @testset "Data format: JSON" begin
         data = daily(datatype="json", apikey=ENV["ALPHA_VANTAGE_API_KEY"])
         @test typeof(data) === Dict{String,Any}
@@ -39,6 +94,7 @@ end
         @test typeof(data) === Array{Any, 2}
         @test size(data) === (101, 9)
     end
+
     @testset "Data format: JSON" begin
         data = daily_adjusted(datatype="json", apikey=ENV["ALPHA_VANTAGE_API_KEY"])
         @test typeof(data) === Dict{String,Any}
@@ -55,6 +111,7 @@ end
         @test typeof(data) === Array{Any, 2}
         @test size(data) === (930, 6)
     end
+
     @testset "Data format: JSON" begin
         data = weekly(datatype="json", apikey=ENV["ALPHA_VANTAGE_API_KEY"])
         @test typeof(data) === Dict{String,Any}
@@ -71,6 +128,7 @@ end
         @test typeof(data) === Array{Any, 2}
         @test size(data) === (930, 8)
     end
+
     @testset "Data format: JSON" begin
         data = weekly_adjusted(datatype="json", apikey=ENV["ALPHA_VANTAGE_API_KEY"])
         @test typeof(data) === Dict{String,Any}
@@ -87,6 +145,7 @@ end
         @test typeof(data) === Array{Any, 2}
         @test size(data) === (214, 6)
     end
+
     @testset "Data format: JSON" begin
         data = monthly(datatype="json", apikey=ENV["ALPHA_VANTAGE_API_KEY"])
         @test typeof(data) === Dict{String,Any}
@@ -103,6 +162,7 @@ end
         @test typeof(data) === Array{Any, 2}
         @test size(data) === (214, 8)
     end
+
     @testset "Data format: JSON" begin
         data = monthly_adjusted(datatype="json", apikey=ENV["ALPHA_VANTAGE_API_KEY"])
         @test typeof(data) === Dict{String,Any}
