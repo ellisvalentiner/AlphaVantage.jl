d0fecc37e0961d042aa251adbb592fff9188432b
Ellis Valentiner <ellisvalentiner@users.noreply.github.com>
2017-11-03 22:16:08 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -13,8 +13,5 @@ makedocs(
 deploydocs(
     deps   = Deps.pip("mkdocs", "python-markdown-math"),
     repo   = "github.com/ellisvalentiner/AlphaVantage.jl.git",
-    julia  = "0.6",
-    osname = "osx",
-    deps   = nothing,
-    make   = nothing
+    julia  = "0.6"
 )
