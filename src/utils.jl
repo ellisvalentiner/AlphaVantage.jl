96be5b81e0a45374e7696709be10793901758c06
dm13450 <dean.markwick@talk21.com>
2020-06-22 08:18:02 +0100

    Bug fix and uri former

--- src/utils.jl
+++ src/utils.jl
@@ -5,8 +5,8 @@ Internal function that wraps Requests.get
 function _get_request(uri::String)
     resp = HTTP.get(uri)
     if resp.status != 200
-        desc = STATUS_CODES[status]
-        error("Expected status code 200 but received $status: $desc")
+        #desc = STATUS_CODES[status]
+        error("Expected status code 200 but received $resp.status")
     end
     return resp
 end
@@ -20,4 +20,19 @@ function _parse_response(data, datatype::String)
     elseif datatype == "json"
         return JSON.Parser.parse(String(data.body))
     end
+end
+
+"""
+Internal function that forms the request uri
+"""
+function _form_uri_stock_timeseries(func::String, symbol::String, outputsize::String="compact", datatype::String="json")
+
+    uri = "$(alphavantage_api)query?"
+    
+    f = "function="* uppercase(func)
+    s = "symbol=$(symbol)"
+    o = "outputsize=$outputsize"
+    d = "datatype=$datatype"
+    a = "apikey=" * ENV["ALPHA_VANTAGE_API_KEY"]
+    reqString = uri * f * "&" * s * "&" * o * "&" * d * "&" * a 
 end
