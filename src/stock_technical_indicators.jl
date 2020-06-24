84c21ff18f15aac4ac457d9a8944f54efeadef16
dm13450 <dean.markwick@talk21.com>
2020-06-24 08:08:02 +0100

    Add Technical Indicators

--- src/stock_technical_indicators.jl
+++ src/stock_technical_indicators.jl
@@ -1,6 +1,66 @@
-function technical_indicator(func::String; symbol::String="SMA", interval::String="1min", time_period::Integer=60, series_type::String="close")
-    uri = "$(alphavantage_api)query?function=$func&from_currency=$from_currency&to_currency=$to_currency&apikey=" * ENV["ALPHA_VANTAGE_API_KEY"]
-    data = _get_request(uri)
-    return _parse_response(data, datatype)
+interval_indicators = ["VWAP", "AD", "OBV"]
+
+for func in interval_indicators
+    fname = Symbol(func)
+    @eval begin
+        function ($fname)(symbol::String, interval::String, datatype::String="json")
+            @argcheck in(interval, ["1min", "5min", "15min", "30min", "60min", "daily", "weekly", "monthly"])
+            uri = _form_uri_head(uppercase($func)) * "&symbol=$symbol&interval=$interval&apikey=" * ENV["ALPHA_VANTAGE_API_KEY"]
+            data = _get_request(uri)
+            return _parse_response(data, datatype)
+        end
+    export $fname
+    end
 end
-export technical_indicator
+
+interval_seriestype_indicators = ["MACD"]
+
+for func in interval_seriestype_indicators
+    fname = Symbol(func)
+    @eval begin
+        function ($fname)(symbol::String, interval::String, series_type::String, datatype::String="json")
+            @argcheck in(interval, ["1min", "5min", "15min", "30min", "60min", "daily", "weekly", "monthly"])
+            @argcheck in(series_type, ["open", "high", "low", "close"])
+
+            uri = _form_uri_head(uppercase($func)) * "&symbol=$symbol&interval=$interval&series_type=$series_type&apikey=" * ENV["ALPHA_VANTAGE_API_KEY"]
+            data = _get_request(uri)
+            return _parse_response(data, datatype)
+        end
+    export $fname
+    end
+end
+
+interval_timeperiod_indicators = ["ADX", "ADXR", "CCI", "AROON"]
+
+for func in interval_timeperiod_indicators
+    fname = Symbol(func)
+    @eval begin
+        function ($fname)(symbol::String, interval::String, time_period::Int64, datatype::String="json")
+            @argcheck in(interval, ["1min", "5min", "15min", "30min", "60min", "daily", "weekly", "monthly"])
+            @argcheck time_period > 0
+    
+            uri = _form_uri_head(uppercase($func)) * "&symbol=$symbol&interval=$interval&time_period=$time_period&apikey=" * ENV["ALPHA_VANTAGE_API_KEY"]
+            data = _get_request(uri)
+            return _parse_response(data, datatype)
+        end
+        export $fname
+    end
+end
+
+interval_timeperiod_seriestype_indicators = ["EMA", "SMA", "WMA", "DEMA", "TEMA", "TRIMA", "KAMA", "RSI"]
+
+for func in interval_timeperiod_seriestype_indicators
+    fname = Symbol(func)
+    @eval begin 
+        function ($fname)(symbol::String, interval::String, time_period::Int64, series_type::String, datatype::String="json")
+            @argcheck in(interval, ["1min", "5min", "15min", "30min", "60min", "daily", "weekly", "monthly"])
+            @argcheck in(series_type, ["open", "high", "low", "close"])
+            @argcheck time_period > 0
+
+            uri = _form_uri_head(uppercase($func)) * "&symbol=$symbol&interval=$interval&time_period=$time_period&series_type=$series_type&apikey=" * ENV["ALPHA_VANTAGE_API_KEY"]
+            data = _get_request(uri)
+            return _parse_response(data, datatype)
+        end
+    export $fname
+    end
+end
