ce4b81ed3912282d75340de01fc94b30205ebbe3
Tom Breloff <tom@breloff.com>
2020-08-07 12:54:07 -0400

    added fundamentals

---
+++ src/fundamentals.jl
@@ -0,0 +1,44 @@
+
+# Company Overview (https://www.alphavantage.co/documentation/#company-overview)
+# ex: https://www.alphavantage.co/query?function=OVERVIEW&symbol=IBM&apikey=demo
+
+function company_overview(symbol::String; outputsize::String="compact", datatype::String="json")
+    @argcheck in(datatype, ["json", "csv"])
+    uri = _form_uri_head("OVERVIEW") * "&symbol=$symbol" * _form_uri_tail(outputsize, datatype)
+    data = _get_request(uri)
+    return _parse_response(data, datatype)
+end
+export company_overview
+
+# Income Statement (https://www.alphavantage.co/documentation/#income-statement)
+# ex: https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol=IBM&apikey=demo
+
+function income_statement(symbol::String; outputsize::String="compact", datatype::String="json")
+    @argcheck in(datatype, ["json", "csv"])
+    uri = _form_uri_head("INCOME_STATEMENT") * "&symbol=$symbol" * _form_uri_tail(outputsize, datatype)
+    data = _get_request(uri)
+    return _parse_response(data, datatype)
+end
+export income_statement
+
+# Balance Sheet (https://www.alphavantage.co/documentation/#balance-sheet)
+# ex: https://www.alphavantage.co/query?function=BALANCE_SHEET&symbol=IBM&apikey=demo
+
+function balance_sheet(symbol::String; outputsize::String="compact", datatype::String="json")
+    @argcheck in(datatype, ["json", "csv"])
+    uri = _form_uri_head("BALANCE_SHEET") * "&symbol=$symbol" * _form_uri_tail(outputsize, datatype)
+    data = _get_request(uri)
+    return _parse_response(data, datatype)
+end
+export balance_sheet
+
+# Cash Flow (https://www.alphavantage.co/documentation/#cash-flow)
+# ex: https://www.alphavantage.co/query?function=CASH_FLOW&symbol=IBM&apikey=demo
+
+function cash_flow(symbol::String; outputsize::String="compact", datatype::String="json")
+    @argcheck in(datatype, ["json", "csv"])
+    uri = _form_uri_head("CASH_FLOW") * "&symbol=$symbol" * _form_uri_tail(outputsize, datatype)
+    data = _get_request(uri)
+    return _parse_response(data, datatype)
+end
+export cash_flow
