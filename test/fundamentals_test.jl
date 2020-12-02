022d9b80072958ecf5ecab41a2d8a546289a183a
Jens Herold <Jens.Herold@uni-bonn.de>
2020-12-02 21:28:29 +0100

    Added "earnings" function to fundamentals.jl (#25)
    * Added "earnings" function to fundamentals.jl
    
    * added test for "earnings" in fundamentals_test.jl

--- test/fundamentals_test.jl
+++ test/fundamentals_test.jl
@@ -1,29 +1,36 @@
 @testset "Fundamentals" begin
     symbol = "IBM"
 
-    @testset "Overview" begin 
+    @testset "Overview" begin
         data = company_overview(symbol)
         @test typeof(data) === Dict{String, Any}
         @test length(data) === 59
         sleep(TEST_SLEEP_TIME + 2*rand()) #as to not overload the API
     end
-    
-    @testset "Income Statement" begin 
+
+    @testset "Income Statement" begin
         data = income_statement(symbol)
         @test typeof(data) === Dict{String, Any}
         @test length(data) === 3
         sleep(TEST_SLEEP_TIME + 2*rand()) #as to not overload the API
     end
-    
-    @testset "Balance Sheet" begin 
+
+    @testset "Balance Sheet" begin
         data = balance_sheet(symbol)
         @test typeof(data) === Dict{String, Any}
         @test length(data) === 3
         sleep(TEST_SLEEP_TIME + 2*rand()) #as to not overload the API
     end
-    
-    @testset "Cash Flow" begin 
+
+    @testset "Cash Flow" begin
         data = cash_flow(symbol)
+        @test typeof(data) === Dict{String, Any}
+        @test length(data) === 3
+        sleep(TEST_SLEEP_TIME + 2*rand()) #as to not overload the API
+    end
+
+    @testset "Earnings" begin
+        data = earnings(symbol)
         @test typeof(data) === Dict{String, Any}
         @test length(data) === 3
         sleep(TEST_SLEEP_TIME + 2*rand()) #as to not overload the API
