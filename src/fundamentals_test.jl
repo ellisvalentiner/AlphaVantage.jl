fb72f9a7858d5ee94552ebc79104ec059ccc91e8
Jens Herold <Jens.Herold@uni-bonn.de>
2020-12-02 16:02:50 +0100

    added test for "earnings" function

---
+++ src/fundamentals_test.jl
@@ -0,0 +1,38 @@
+@testset "Fundamentals" begin
+    symbol = "IBM"
+
+    @testset "Overview" begin
+        data = company_overview(symbol)
+        @test typeof(data) === Dict{String, Any}
+        @test length(data) === 59
+        sleep(TEST_SLEEP_TIME + 2*rand()) #as to not overload the API
+    end
+
+    @testset "Income Statement" begin
+        data = income_statement(symbol)
+        @test typeof(data) === Dict{String, Any}
+        @test length(data) === 3
+        sleep(TEST_SLEEP_TIME + 2*rand()) #as to not overload the API
+    end
+
+    @testset "Balance Sheet" begin
+        data = balance_sheet(symbol)
+        @test typeof(data) === Dict{String, Any}
+        @test length(data) === 3
+        sleep(TEST_SLEEP_TIME + 2*rand()) #as to not overload the API
+    end
+
+    @testset "Cash Flow" begin
+        data = cash_flow(symbol)
+        @test typeof(data) === Dict{String, Any}
+        @test length(data) === 3
+        sleep(TEST_SLEEP_TIME + 2*rand()) #as to not overload the API
+    end
+
+    @testset "Earnings" begin
+        data = earnings(symbol)
+        @test typeof(data) === Dict{String, Any}
+        @test length(data) === 3
+        sleep(TEST_SLEEP_TIME + 2*rand()) #as to not overload the API
+    end
+end
