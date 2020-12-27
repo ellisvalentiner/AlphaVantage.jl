153a4974c16c0b05ddb95c0dbaa944e6ca96f5d6
RocketRoss <code@radonn.co.za>
2020-12-27 11:05:03 +0200

    +   time_series_intraday_extended

--- src/stock_time_series.jl
+++ src/stock_time_series.jl
@@ -1,3 +1,17 @@
+function time_series_intraday_extended(symbol::String, interval::String="60min", slice::String="year1month1"; datatype::String="json")
+    @argcheck in(interval, ["1min", "5min", "15min", "30min", "60min"])
+    @argcheck all(contains.(slice, ["year", "month"]))
+    @argcheck length(findall(r"[\d]+", slice)) == 2
+    @argcheck tryparse(Int, slice[findall(r"[\d]+", slice)[1]]) > 0
+    @argcheck tryparse(Int, slice[findall(r"[\d]+", slice)[2]]) > 0
+    @argcheck tryparse(Int, slice[findall(r"[\d]+", slice)[2]]) < 13
+    @argcheck in(datatype, ["json", "csv"])
+    uri = "$(alphavantage_api)query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=$symbol&interval=$interval&slice=$slice&datatype=$datatype&apikey=" * ENV["ALPHA_VANTAGE_API_KEY"]
+    data = _get_request(uri)
+    return _parse_response(data, datatype)
+end
+export time_series_intraday_extended
+
 function time_series_intraday(symbol::String, interval::String="1min"; outputsize::String="compact", datatype::String="json")
     @argcheck in(interval, ["1min", "5min", "15min", "30min", "60min"])
     @argcheck in(outputsize, ["compact", "full"])
