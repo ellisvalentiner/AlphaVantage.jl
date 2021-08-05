56c86f6db3c517fbc4de7fb3185a3ee40189c678
Ellis Valentiner <ellisvalentiner@gmail.com>
2021-08-05 09:08:22 -0400

    test: add missing tests for api limit check

---
+++ test/utils_test.jl
@@ -0,0 +1,17 @@
+module UtilsTest
+using AlphaVantage
+using HTTP
+using Test
+
+@testset "Utils" begin
+    resp = HTTP.Messages.Response(
+        200,
+        [
+            Pair("Content-Type", "application/json")
+        ];
+        body="""{"Note": "API limit exceeded"}"""
+    )
+    @test_throws Exception AlphaVantage._check_api_limit(resp)
+end
+
+end
